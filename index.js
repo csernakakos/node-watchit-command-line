6506a0580a3b8c1d5fe9e1beaa8c11e8b4dbb273
csernakakos <csernakakos@gmail.com>
2022-02-14 18:50:07 +0100

    Finished project

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 #!/usr/bin/env node
+const fs = require("fs");
 const debounce = require("lodash.debounce");
 const chokidar = require("chokidar");
 const program = require("caporal");
+const {spawn} = require("child_process");
+const chalk = require("chalk");
 
-const start = debounce(() => {console.log("starting...");}, 100);
 
-chokidar
-    .watch(".")
-    .on("add", start)
-    .on("change", () => {console.log("File changed")})
-    .on("unlink", () => {console.log("File unlinked")})
+
+program
+    .version("0.0.1")
+    .argument("[filename]", "Name of file to execute")
+    .action(async function({filename}){
+
+        const name = filename || "index.js";
+        try {
+            await fs.promises.access(name)
+        } catch (err) {
+            throw new Error(`${name} doesn't exist.`);
+        }
+
+        let proc;
+        const start = debounce(() => {
+            if (proc) {
+                proc.kill();
+            };
+            console.log(chalk.bold.blue(">>>> Starting process..."));
+            proc = spawn("node", [name], {stdio: "inherit"});
+        }, 100);
+
+        chokidar
+            .watch(".")
+            .on("add", start)
+            .on("change", start)
+            .on("unlink", start);
+    });
+
+program.parse(process.argv);
